42bf8ffe95e4cd1dff26759f377484d794eb1a4c
Aries <10256477+ariesclark@users.noreply.github.com>
2024-07-15 16:51:49 -0600

    fix(tailwindcss): reinclude callees

--- src/configs/tailwindcss.ts
+++ src/configs/tailwindcss.ts
@@ -2,13 +2,28 @@ import { config, type Config } from "typescript-eslint";
 // @ts-expect-error: types not available.
 import tailwindcss from "eslint-plugin-tailwindcss";
 
+const callees = ["classnames", "clsx", "ctl", "cva", "twMerge", "tv"];
+
 export default config({
 	files: ["**/*.{js,jsx,ts,tsx}"],
 	name: "@ariesclark/eslint-config/tailwindcss",
 	plugins: { tailwindcss },
 	rules: {
 		"tailwindcss/classnames-order": "warn",
-		"tailwindcss/no-contradicting-classname": "warn",
-		"tailwindcss/no-custom-classname": "warn"
+		"tailwindcss/enforces-negative-arbitrary-values": "warn",
+		"tailwindcss/enforces-shorthand": "warn",
+		"tailwindcss/migration-from-tailwind-2": "off",
+		"tailwindcss/no-arbitrary-value": "off",
+		"tailwindcss/no-contradicting-classname": "error",
+		"tailwindcss/no-custom-classname": "warn",
+		"tailwindcss/no-unnecessary-arbitrary-value": "warn"
+	},
+	settings: {
+		tailwindcss: {
+			callees,
+			cssFilesRefreshRate: 5_000,
+			removeDuplicates: true,
+			skipClassAttribute: false
+		}
 	}
 }) as Config;
